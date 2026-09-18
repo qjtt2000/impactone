d4f5fe5f0941bbc33c4b850a3b0ceb34b6b6f2e1
qjtt2000 <jqi8072@monroeu.edu>
2026-09-18 19:16:47 -0400

    Update wechat-generate-link.js

--- api/wechat-generate-link.js
+++ api/wechat-generate-link.js
@@ -7,8 +7,8 @@ export default async function handler(req, res) {
   }
 
   try {
-    const appid = process.env.WECHAT_APPID;
-    const secret = process.env.WECHAT_SECRET;
+   const appid = process.env.WECHAT_MINIPROGRAM_APPID;
+const secret = process.env.WECHAT_MINIPROGRAM_APPSECRET;
 
     if (!appid || !secret) {
       return res.status(500).json({
